9c5ba50cc7d1833b0a22dc3f175f87236e498ec4
Roman Volosatovs <rvolosatovs@riseup.net>
2023-03-21 15:57:40 +0100

    feat(lib): support more attrs in `extendDerivations`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- lib/extendDerivations.nix
+++ lib/extendDerivations.nix
@@ -7,11 +7,14 @@ with nixlib.lib;
 with builtins;
   {
     buildInputs ? [],
+    inputsFrom ? [],
     nativeBuildInputs ? [],
+    packages ? [],
   }:
     mapAttrs (n: v:
       v.overrideAttrs (attrs: {
-        buildInputs = attrs.buildInputs ++ buildInputs;
-
-        nativeBuildInputs = attrs.nativeBuildInputs ++ nativeBuildInputs;
+        buildInputs = (attrs.buildInputs or []) ++ buildInputs;
+        inputsFrom = (attrs.inputsFrom or []) ++ inputsFrom;
+        nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ nativeBuildInputs;
+        packages = (attrs.packages or []) ++ packages;
       }))
