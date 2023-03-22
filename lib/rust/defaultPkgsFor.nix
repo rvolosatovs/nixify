c6136479551d3da5367aee0c372444c68f4bc272
Roman Volosatovs <rvolosatovs@riseup.net>
2023-03-22 16:50:10 +0100

    feat(rust): account for win64 targets
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- lib/rust/defaultPkgsFor.nix
+++ lib/rust/defaultPkgsFor.nix
@@ -10,17 +10,21 @@ with flake-utils.lib.system;
     then pkgs
     else if pkgs.hostPlatform.system == aarch64-linux && target == "aarch64-unknown-linux-musl"
     then pkgs
+    else if pkgs.hostPlatform.system == x86_64-windows && target == "x86_64-pc-windows-gnu"
+    then pkgs
     else if pkgs.hostPlatform.system == x86_64-linux && target == "x86_64-unknown-linux-musl"
     then pkgs
     else if target == "aarch64-unknown-linux-musl"
     then pkgs.pkgsCross.aarch64-multiplatform
     else if target == "aarch64-apple-darwin"
     then pkgs.pkgsCross.aarch64-darwin
-    else if target == "x86_64-unknown-linux-musl"
-    then pkgs.pkgsCross.gnu64
     else if target == "x86_64-apple-darwin"
     then pkgs.pkgsCross.x86_64-darwin
-    else if target == "wasm32-wasi"
+    else if target == "x86_64-pc-windows-gnu"
+    then pkgs.pkgsCross.mingwW64
+    else if target == "x86_64-unknown-linux-musl"
+    then pkgs.pkgsCross.gnu64
+    else if target == wasm32-wasi
     then pkgs.pkgsCross.wasi32
     else
       import nixpkgs {
