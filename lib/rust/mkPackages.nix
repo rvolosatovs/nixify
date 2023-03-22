623f8be10bcd4d8fc7f9f71857d3830a437a0f6f
Roman Volosatovs <rvolosatovs@riseup.net>
2023-03-22 12:02:41 +0100

    feat(rust): add support for `doc` config
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- lib/rust/mkPackages.nix
+++ lib/rust/mkPackages.nix
@@ -5,6 +5,7 @@ with self.lib.rust;
     buildOverrides ? defaultBuildOverrides,
     cargoLock ? null,
     clippy ? defaultClippyConfig,
+    doc ? defaultDocConfig,
     doCheck ? true,
     pkgsFor ? defaultPkgsFor,
     pname ? null,
