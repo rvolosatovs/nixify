ca05b061d74f5084b0e87705456ed34f16076afc
Roman Volosatovs <rvolosatovs@riseup.net>
2023-05-04 16:45:30 +0200

    feat: allow extending shell env
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- lib/extendDerivations.nix
+++ lib/extendDerivations.nix
@@ -8,15 +8,20 @@ with builtins;
   {
     buildInputs ? [],
     depsBuildBuild ? [],
+    env ? null,
     inputsFrom ? [],
     nativeBuildInputs ? [],
     packages ? [],
   }:
     mapAttrs (n: v:
-      v.overrideAttrs (attrs: {
-        buildInputs = (attrs.buildInputs or []) ++ buildInputs;
-        depsBuildBuild = (attrs.depsBuildBuild or []) ++ depsBuildBuild;
-        inputsFrom = (attrs.inputsFrom or []) ++ inputsFrom;
-        nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ nativeBuildInputs;
-        packages = (attrs.packages or []) ++ packages;
-      }))
+      v.overrideAttrs (attrs:
+        {
+          buildInputs = (attrs.buildInputs or []) ++ buildInputs;
+          depsBuildBuild = (attrs.depsBuildBuild or []) ++ depsBuildBuild;
+          inputsFrom = (attrs.inputsFrom or []) ++ inputsFrom;
+          nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ nativeBuildInputs;
+          packages = (attrs.packages or []) ++ packages;
+        }
+        // optionalAttrs (env != null) {
+          env = (attrs.env or {}) // env;
+        }))
