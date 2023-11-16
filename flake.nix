f313cdb5e909f31efd9736ec32321e75dc92047c
Roman Volosatovs <rvolosatovs@riseup.net>
2023-11-16 18:18:17 +0100

    build: add `nixify` cache
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   nixConfig.extra-substituters = [
+    "https://nixify.cachix.org"
     "https://crane.cachix.org"
     "https://nix-community.cachix.org"
     "https://cache.garnix.io"
   ];
   nixConfig.extra-trusted-public-keys = [
+    "nixify.cachix.org-1:95SiUQuf8Ij0hwDweALJsLtnMyv/otZamWNRp1Q1pXw="
     "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
@@ -13,20 +15,20 @@
   description = "Simple, yet extensible nix flake bootstrapping library for real-world projects";
 
   inputs.advisory-db.flake = false;
-  inputs.advisory-db.url = github:rustsec/advisory-db;
-  inputs.crane.url = github:ipetkov/crane/v0.15.0;
+  inputs.advisory-db.url = "github:rustsec/advisory-db";
+  inputs.crane.url = "github:ipetkov/crane/v0.15.0";
   inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.fenix.url = github:nix-community/fenix;
-  inputs.flake-utils.url = github:numtide/flake-utils;
-  inputs.macos-sdk.url = https://github.com/roblabla/MacOSX-SDKs/releases/download/macosx14.0/MacOSX14.0.sdk.tar.xz;
+  inputs.fenix.url = "github:nix-community/fenix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.macos-sdk.url = "https://github.com/roblabla/MacOSX-SDKs/releases/download/macosx14.0/MacOSX14.0.sdk.tar.xz";
   inputs.macos-sdk.flake = false;
-  inputs.nix-filter.url = github:numtide/nix-filter;
-  inputs.nix-log.url = github:rvolosatovs/nix-log;
-  inputs.nixlib.url = github:nix-community/nixpkgs.lib;
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-23.05;
+  inputs.nix-filter.url = "github:numtide/nix-filter";
+  inputs.nix-log.url = "github:rvolosatovs/nix-log";
+  inputs.nixlib.url = "github:nix-community/nixpkgs.lib";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
   inputs.rust-overlay.inputs.flake-utils.follows = "flake-utils";
   inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.rust-overlay.url = github:oxalica/rust-overlay;
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
   outputs = inputs: let
     lib = import ./lib inputs;
