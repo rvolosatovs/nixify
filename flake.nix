e786cbf699de7b1327de68aabbc7fe3604842dac
Roman Volosatovs <rvolosatovs@riseup.net>
2023-12-05 14:27:08 +0100

    feat: use `-darwin` package set on Darwin
    This will hopefully maximize cache hits
    
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
   inputs.advisory-db.flake = false;
   inputs.advisory-db.url = "github:rustsec/advisory-db";
   inputs.crane.url = "github:ipetkov/crane/v0.15.0";
-  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs-nixos";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.macos-sdk.url = "https://github.com/roblabla/MacOSX-SDKs/releases/download/macosx14.0/MacOSX14.0.sdk.tar.xz";
@@ -25,10 +25,11 @@
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.nix-log.url = "github:rvolosatovs/nix-log";
   inputs.nixlib.url = "github:nix-community/nixpkgs.lib";
+  inputs.nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
   inputs.nixpkgs-jshon.url = "github:nixos/nixpkgs/89023fc074c2333ec5f1d28075602c94341655d2"; # https://github.com/NixOS/nixpkgs/pull/272259
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  inputs.nixpkgs-nixos.url = "github:nixos/nixpkgs/nixos-23.11";
   inputs.rust-overlay.inputs.flake-utils.follows = "flake-utils";
-  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs-nixos";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
   outputs = inputs: let
