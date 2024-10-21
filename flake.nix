0e0bc1c472d0f151ded55dc512ad208594c57cd9
Roman Volosatovs <rvolosatovs@riseup.net>
2024-10-21 15:18:34 +0200

    build(nix): unpin `crane`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
   inputs.advisory-db.flake = false;
   inputs.advisory-db.url = "github:rustsec/advisory-db";
-  inputs.crane.url = "github:ipetkov/crane/v0.19.1";
+  inputs.crane.url = "github:ipetkov/crane";
   inputs.fenix.inputs.nixpkgs.follows = "nixpkgs-nixos";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.flake-utils.url = "github:numtide/flake-utils";
